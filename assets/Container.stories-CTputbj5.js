d38106f9de156b15fc90a55bde26682f514346f3
Oleh Levchenko <leva13007@gmail.com>
2025-11-19 00:19:14 +0000

    Cleanup: clear old storybook build

--- assets/Container.stories-CTputbj5.js
+++
@@ -1,5 +0,0 @@
-import{C as r}from"./Container-TRQm39Ac.js";import"./iframe-Blp878KL.js";import"./preload-helper-PPVm8Dsz.js";const a={title:"UI/Atoms/Container",component:r,tags:["autodocs"],args:{children:"Some inner Text"},argTypes:{children:{control:{type:"text"}},fluid:{control:{type:"boolean"}}}},e={args:{children:"Title"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: 'Title'
-  }
-}`,...e.parameters?.docs?.source}}};const s=["Element"];export{e as Element,s as __namedExportsOrder,a as default};
